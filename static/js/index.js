1762adbc04994584907826e6c5db67b1b1c1917d
SysuCharon <lu_charon@qq.com>
2016-11-24 22:14:53 +0800

    add ajax

--- static/js/index.js
+++ static/js/index.js
@@ -1,63 +1,74 @@
 $(function(){
 	// nav active
 	var pgurl = window.location.href.split('/')
+    var pageContent = ''
 	if(pgurl.length > 4) {
-		temp = pgurl[pgurl.length - 2]
-	} else {
-		temp = ''
+		pageContent = pgurl[pgurl.length - 2]
 	}
-	var active = '/' + temp
+
+	var active = '/' + pageContent
 	$("nav a").each(function(){
 		if($(this).attr("href") == active)
 			$(this).addClass("active");
 	})
 
-	$("#login_form").validate({  
-        rules: {  
-            username: "required",  
-            password: {  
-                required: true,  
-                minlength: 5  
-            },  
-        },  
-        messages: {  
-            username: "请输入姓名",  
-            password: {  
-                required: "请输入密码",  
-                minlength: jQuery.format("密码不能小于{0}个字 符")  
-            },  
-        }  
-    });  
-    $("#register_form").validate({  
-        rules: {  
-            username: "required",  
-            password: {  
-                required: true,  
-                minlength: 5  
-            },  
-            rpassword: {  
-                equalTo: "#register_password"  
-            },  
-            email: {  
-                required: true,  
-                email: true  
-            }  
-        },  
-        messages: {  
-            username: "请输入姓名",  
-            password: {  
-                required: "请输入密码",  
-                minlength: jQuery.format("密码不能小于{0}个字 符")  
-            },  
-            rpassword: {  
-                equalTo: "两次密码不一样"  
-            },  
-            email: {  
-                required: "请输入邮箱",  
-                email: "请输入有效邮箱"  
-            }  
-        }  
-    }); 
+    $(".get").click(function(event){
+        event.preventDefault();
+        var data_id = $(this).attr('data-id')
+        console.log(data_id)
+        url = '/' + pageContent + 'GetContent'
+        console.log(url)
+        $.get(url, {'data_id': data_id}, function(ret){
+            $("#content").html(ret)
+        })
+    })
+
+	// $("#login_form").validate({  
+ //        rules: {  
+ //            username: "required",  
+ //            password: {  
+ //                required: true,  
+ //                minlength: 5  
+ //            },  
+ //        },  
+ //        messages: {  
+ //            username: "请输入姓名",  
+ //            password: {  
+ //                required: "请输入密码",  
+ //                minlength: jQuery.format("密码不能小于{0}个字 符")  
+ //            },  
+ //        }  
+ //    });  
+ //    $("#register_form").validate({  
+ //        rules: {  
+ //            username: "required",  
+ //            password: {  
+ //                required: true,  
+ //                minlength: 5  
+ //            },  
+ //            rpassword: {  
+ //                equalTo: "#register_password"  
+ //            },  
+ //            email: {  
+ //                required: true,  
+ //                email: true  
+ //            }  
+ //        },  
+ //        messages: {  
+ //            username: "请输入姓名",  
+ //            password: {  
+ //                required: "请输入密码",  
+ //                minlength: jQuery.format("密码不能小于{0}个字 符")  
+ //            },  
+ //            rpassword: {  
+ //                equalTo: "两次密码不一样"  
+ //            },  
+ //            email: {  
+ //                required: "请输入邮箱",  
+ //                email: "请输入有效邮箱"  
+ //            }  
+ //        }  
+ //    }); 
 
     
 })
